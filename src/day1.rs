86abebc1f40db260ac2d8e8110d1bc76c4e9ee39
Tim Peters <tim@catminion.net>
2021-12-01 21:05:17 +1030

    Add day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,7 @@
 pub fn run(input: &str) {
     let input = parse_input(input);
-    println!("Day 1, part one: {}", count_increases(input));
+    println!("Day 1, part one: {}", count_increases(&input));
+    println!("Day 1, part two: {}", count_sliding_increases(&input));
 }
 
 fn parse_input(input: &str) -> Vec<i32> {
@@ -10,10 +11,24 @@ fn parse_input(input: &str) -> Vec<i32> {
         .collect()
 }
 
-fn count_increases(input: Vec<i32>) -> i32 {
+fn count_increases(input: &Vec<i32>) -> i32 {
     let mut count = 0;
     for i in 1..input.len() {
         if input[i] > input[i - 1] {
+            count += 1
+        }
+    }
+    count
+}
+
+fn count_sliding_increases(input: &Vec<i32>) -> i32 {
+    let mut count = 0;
+    let mut sum = input[0] + input[1] + input[2]; // first window
+    for i in 1..input.len() - 2 {
+        let lastsum = sum;
+        sum -= input[i - 1]; // take out first number of previous window
+        sum += input[i + 2]; // add in last number of this window
+        if sum > lastsum {
             count += 1
         }
     }
@@ -24,6 +39,14 @@ fn count_increases(input: Vec<i32>) -> i32 {
 fn test_count_increases() {
     assert_eq!(
         7,
-        count_increases(vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+        count_increases(&vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
+    )
+}
+
+#[test]
+fn test_count_sliding_increases() {
+    assert_eq!(
+        5,
+        count_sliding_increases(&vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263])
     )
 }
