edae786b89de4b0a881b01b6d5e7e8be01bb0bcb
Tim Peters <tim@catminion.net>
2021-12-03 22:44:46 +1030

    Add day 3 part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,10 +1,14 @@
 pub fn run(input: &str) {
     let input = parse_input(&input);
-    let (gamma, epsilon) = calc_power(&input);
+    let (gamma, epsilon) = calc_power_consumption(&input);
     println!("Day 3, part one: {}", gamma * epsilon);
+    println!(
+        "Day 3, part two: {}",
+        calc_oxygen_generator_rating(&input) * calc_co2_scrubber_rating(&input)
+    );
 }
 
-fn calc_power(input: &Vec<usize>) -> (usize, usize) {
+fn calc_power_consumption(input: &Vec<usize>) -> (usize, usize) {
     let mut gamma = 0;
     let mut epsilon = 0;
     let half = input.len() / 2;
@@ -19,6 +23,54 @@ fn calc_power(input: &Vec<usize>) -> (usize, usize) {
         }
     }
     (gamma, epsilon)
+}
+
+fn calc_oxygen_generator_rating(input: &Vec<usize>) -> usize {
+    let mut list = input.clone();
+    for n in 0..=15 {
+        let count = list.iter().filter(|&row| row & (1 << 15 - n) > 0).count();
+        if count > 0 {
+            if count * 2 >= list.len() {
+                list = list
+                    .into_iter()
+                    .filter(|&row| row & (1 << 15 - n) > 0)
+                    .collect::<Vec<usize>>();
+            } else {
+                list = list
+                    .into_iter()
+                    .filter(|&row| row & (1 << 15 - n) == 0)
+                    .collect::<Vec<usize>>();
+            }
+            if list.len() == 1 {
+                return list[0];
+            }
+        }
+    }
+    0
+}
+
+fn calc_co2_scrubber_rating(input: &Vec<usize>) -> usize {
+    let mut list = input.clone();
+    for n in 0..=15 {
+        let count = list.iter().filter(|&row| row & (1 << 15 - n) > 0).count();
+        if count > 0 {
+            if count * 2 >= list.len() {
+                list = list
+                    .into_iter()
+                    .filter(|&row| row & (1 << 15 - n) == 0)
+                    .collect::<Vec<usize>>();
+            } else {
+                list = list
+                    .into_iter()
+                    .filter(|&row| row & (1 << 15 - n) > 0)
+                    .collect::<Vec<usize>>();
+            }
+            if list.len() == 1 {
+                return list[0];
+            }
+        }
+    }
+    0
 }
 
 fn parse_input(input: &str) -> Vec<usize> {
@@ -44,5 +96,7 @@ fn test_calc() {
 01010
 ";
     let input = parse_input(&test_input);
-    assert_eq!((22, 9), calc_power(&input));
+    assert_eq!((22, 9), calc_power_consumption(&input));
+    assert_eq!(23, calc_oxygen_generator_rating(&input));
+    assert_eq!(10, calc_co2_scrubber_rating(&input));
 }
