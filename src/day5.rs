18977568965dcf7c7ceeb3038f5c61378e2c4915
Tim Peters <tim@catminion.net>
2021-12-05 18:56:08 +1030

    Add day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -3,7 +3,14 @@ use std::fmt;
 
 pub fn run(input: &str) {
     let grid = parse_input(input);
-    println!("Day 5, part one: {}", grid.count_overlapping_points());
+    println!(
+        "Day 5, part one: {}",
+        grid.count_overlapping_points(CountFlag::WithoutDiagonals)
+    );
+    println!(
+        "Day 5, part two: {}",
+        grid.count_overlapping_points(CountFlag::WithDiagonals)
+    );
 }
 
 #[derive(PartialEq)]
@@ -27,41 +34,101 @@ impl fmt::Debug for Pos {
 struct Line {
     start: Pos,
     end: Pos,
+    dir: Direction,
+}
+
+#[derive(Debug)]
+enum Direction {
+    Horizontal,
+    Vertical,
+    DiagUpRight,
+    DiagDownRight,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+enum CountFlag {
+    WithDiagonals,
+    WithoutDiagonals,
 }
 
 impl Line {
-    fn horizontal(&self) -> bool {
-        self.start.y == self.end.y
-    }
-
-    fn vertical(&self) -> bool {
-        self.start.x == self.end.x
-    }
-
-    fn covers(&self, pos: Pos) -> bool {
-        if self.horizontal() {
-            pos.y == self.start.y
-                && if self.end.x >= self.start.x {
-                    pos.x >= self.start.x && pos.x <= self.end.x
-                } else {
-                    pos.x >= self.end.x && pos.x <= self.start.x
+    // Normalise lines so that x only increases.
+    // y only increases except when it's an up diagonal
+    fn new(start: Pos, end: Pos) -> Line {
+        if start.x == end.x {
+            Line {
+                start: Pos::new(start.x, start.y.min(end.y)),
+                end: Pos::new(end.x, start.y.max(end.y)),
+                dir: Direction::Vertical,
+            }
+        } else if start.y == end.y {
+            Line {
+                start: Pos::new(start.x.min(end.x), start.y),
+                end: Pos::new(start.x.max(end.x), start.y),
+                dir: Direction::Horizontal,
+            }
+        } else if start.x < end.x {
+            if start.y < end.y {
+                // Down and to the right
+                Line {
+                    start: start,
+                    end: end,
+                    dir: Direction::DiagDownRight,
                 }
-        } else if self.vertical() {
-            pos.x == self.start.x
-                && if self.end.y >= self.start.y {
-                    pos.y >= self.start.y && pos.y <= self.end.y
-                } else {
-                    pos.y >= self.end.y && pos.y <= self.start.y
+            } else {
+                // Up and to the right
+                Line {
+                    start: start,
+                    end: end,
+                    dir: Direction::DiagUpRight,
                 }
+            }
         } else {
-            false
+            if start.y < end.y {
+                // Down and to the left - reverse
+                Line {
+                    start: end,
+                    end: start,
+                    dir: Direction::DiagUpRight,
+                }
+            } else {
+                // Up and to the left - reverse
+                Line {
+                    start: end,
+                    end: start,
+                    dir: Direction::DiagDownRight,
+                }
+            }
         }
+    }
+
+    fn covers(&self, pos: Pos, count_flag: CountFlag) -> bool {
+        pos.x >= self.start.x
+            && pos.x <= self.end.x
+            && match self.dir {
+                Direction::Vertical => {
+                    pos.x == self.start.x && pos.y >= self.start.y && pos.y <= self.end.y
+                }
+                Direction::Horizontal => pos.y == self.start.y,
+                Direction::DiagUpRight => {
+                    count_flag == CountFlag::WithDiagonals
+                        && pos.y >= self.end.y
+                        && pos.y <= self.start.y
+                        && pos.x - self.start.x == self.start.y - pos.y
+                }
+                Direction::DiagDownRight => {
+                    count_flag == CountFlag::WithDiagonals
+                        && pos.y >= self.start.y
+                        && pos.y <= self.end.y
+                        && pos.x - self.start.x == pos.y - self.start.y
+                }
+            }
     }
 }
 
 impl fmt::Debug for Line {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?} -> {:?}", self.start, self.end)
+        write!(f, "{:?} -> {:?} ({:?})", self.start, self.end, self.dir)
     }
 }
 
@@ -78,9 +145,13 @@ impl Grid {
         for line in lines.iter() {
             if line.start.x > max.x {
                 max.x = line.start.x;
+            } else if line.end.x > max.x {
+                max.x = line.end.x;
             }
             if line.start.y > max.y {
                 max.y = line.start.y;
+            } else if line.end.y > max.y {
+                max.y = line.end.y;
             }
         }
         Grid {
@@ -90,13 +161,13 @@ impl Grid {
         }
     }
 
-    fn count_overlapping_points(&self) -> usize {
+    fn count_overlapping_points(&self, count_flag: CountFlag) -> usize {
         let mut total = 0;
         for y in self.min.y..=self.max.y {
             for x in self.min.x..=self.max.x {
                 let mut count = 0;
                 for line in self.lines.iter() {
-                    if line.covers(Pos::new(x, y)) {
+                    if line.covers(Pos::new(x, y), count_flag) {
                         count += 1;
                         if count == 2 {
                             total += 1;
@@ -116,15 +187,17 @@ fn parse_input(input: &str) -> Grid {
         input
             .lines()
             .map(|line| re.captures(line).unwrap())
-            .map(|caps| Line {
-                start: Pos::new(
-                    caps.get(1).unwrap().as_str().parse().unwrap(),
-                    caps.get(2).unwrap().as_str().parse().unwrap(),
-                ),
-                end: Pos::new(
-                    caps.get(3).unwrap().as_str().parse().unwrap(),
-                    caps.get(4).unwrap().as_str().parse().unwrap(),
-                ),
+            .map(|caps| {
+                Line::new(
+                    Pos::new(
+                        caps.get(1).unwrap().as_str().parse().unwrap(),
+                        caps.get(2).unwrap().as_str().parse().unwrap(),
+                    ),
+                    Pos::new(
+                        caps.get(3).unwrap().as_str().parse().unwrap(),
+                        caps.get(4).unwrap().as_str().parse().unwrap(),
+                    ),
+                )
             })
             .collect(),
     )
@@ -148,5 +221,9 @@ fn test() {
     assert_eq!(10, grid.lines.len());
     assert_eq!(Pos::new(0, 9), grid.lines[0].start);
     assert_eq!(Pos::new(5, 9), grid.lines[0].end);
-    assert_eq!(5, grid.count_overlapping_points());
+    assert_eq!(
+        5,
+        grid.count_overlapping_points(CountFlag::WithoutDiagonals)
+    );
+    assert_eq!(12, grid.count_overlapping_points(CountFlag::WithDiagonals));
 }
