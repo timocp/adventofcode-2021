844bc81c0f4b80bb447a4dbb19f737d0a1a90710
Tim Peters <tim@catminion.net>
2021-12-13 16:56:28 +1030

    Remove double reads of input files

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::Read;
+use std::slice::Iter;
 use std::time::Instant;
 
 mod day1;
@@ -17,9 +18,17 @@ mod day7;
 mod day8;
 mod day9;
 
+#[derive(Clone, Copy)]
 pub enum Part {
     One,
     Two,
+}
+
+impl Part {
+    pub fn each() -> Iter<'static, Part> {
+        static PARTS: [Part; 2] = [Part::One, Part::Two];
+        PARTS.iter()
+    }
 }
 
 impl fmt::Display for Part {
@@ -36,13 +45,11 @@ fn main() {
 
     if args.len() == 2 {
         let day = args[1].parse().unwrap();
-        run(day, Part::One);
-        run(day, Part::Two);
+        run(day);
     } else {
         let t0 = Instant::now();
         for day in 1..25 {
-            run(day, Part::One);
-            run(day, Part::Two);
+            run(day);
         }
         println!(
             "{:>73} {1:.3}s",
@@ -52,32 +59,34 @@ fn main() {
     }
 }
 
-fn run(day: i32, part: Part) {
+fn run(day: i32) {
     let filename = format!("input/day{}.txt", day);
     match read_file(&filename) {
         Ok(input) => {
-            print!("Day {:02}, part {}:  ", day, part);
-            let t0 = Instant::now();
-            let result = match day {
-                1 => day1::run(&input, part),
-                2 => day2::run(&input, part),
-                3 => day3::run(&input, part),
-                4 => day4::run(&input, part),
-                5 => day5::run(&input, part),
-                6 => day6::run(&input, part),
-                7 => day7::run(&input, part),
-                8 => day8::run(&input, part),
-                9 => day9::run(&input, part),
-                10 => day10::run(&input, part),
-                11 => day11::run(&input, part),
-                12 => day12::run(&input, part),
-                _ => "Not implemented".to_string(),
-            };
-            println!(
-                "{:56} {1:.3}s",
-                result,
-                Instant::now().duration_since(t0).as_secs_f64()
-            );
+            for &part in Part::each() {
+                print!("Day {:02}, part {}:  ", day, part);
+                let t0 = Instant::now();
+                let result = match day {
+                    1 => day1::run(&input, part),
+                    2 => day2::run(&input, part),
+                    3 => day3::run(&input, part),
+                    4 => day4::run(&input, part),
+                    5 => day5::run(&input, part),
+                    6 => day6::run(&input, part),
+                    7 => day7::run(&input, part),
+                    8 => day8::run(&input, part),
+                    9 => day9::run(&input, part),
+                    10 => day10::run(&input, part),
+                    11 => day11::run(&input, part),
+                    12 => day12::run(&input, part),
+                    _ => "Not implemented".to_string(),
+                };
+                println!(
+                    "{:56} {1:.3}s",
+                    result,
+                    Instant::now().duration_since(t0).as_secs_f64()
+                );
+            }
         }
         Err(e) => eprintln!("{}: {}", filename, e),
     }
