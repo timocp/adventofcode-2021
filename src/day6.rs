12faefd300093ce8d634d16350f4b03817a99c77
Tim Peters <tim@catminion.net>
2021-12-16 12:17:28 +1030

    Simplify day 6
    Replace memoized recursion with simple queue.

--- src/day6.rs
+++ src/day6.rs
@@ -1,38 +1,35 @@
 use crate::Part;
-use memoize::memoize;
+use std::collections::VecDeque;
 
 pub fn run(input: &str, part: Part) -> String {
     let input = parse_input(input);
     format!(
         "{}",
-        match part {
-            Part::One => simulate_population(&input, 80),
-            Part::Two => simulate_population(&input, 256),
-        }
+        simulate_population(
+            &input,
+            match part {
+                Part::One => 80,
+                Part::Two => 256,
+            }
+        )
     )
 }
 
-// For a fish with a certain timer, how many descendent fish are there after
-// a number of days?
-#[memoize]
-fn simulate_fish(timer: usize, days: usize) -> usize {
-    let mut timer = timer;
-    let mut days = days;
-    let mut count = 1; // myself
-    while days > 0 {
-        days -= 1;
-        if timer == 0 {
-            count += simulate_fish(8, days); // descendants
-            timer = 6;
-        } else {
-            timer -= 1;
-        }
-    }
-    count
-}
-
 fn simulate_population(fish: &[usize], days: usize) -> usize {
-    fish.iter().map(|&timer| simulate_fish(timer, days)).sum()
+    // queue containing number of fish at each internal timer (0, 1, ... 8),
+    let mut deq: VecDeque<usize> = VecDeque::from(vec![0; 9]);
+    for &timer in fish.iter() {
+        deq[timer] += 1;
+    }
+
+    for _ in 0..days {
+        // anything at timer 0 resets to timer 6 and spawns a new fish at timer 8
+        let breeding = deq.pop_front().unwrap();
+        deq[6] += breeding;
+        deq.push_back(breeding);
+    }
+
+    deq.iter().sum()
 }
 
 fn parse_input(input: &str) -> Vec<usize> {
@@ -46,32 +43,12 @@ fn parse_input(input: &str) -> Vec<usize> {
 }
 
 #[test]
-fn test_day6() {
+fn test() {
     let test_input = "\
 3,4,3,1,2
 ";
     let input = parse_input(test_input);
     assert_eq!(vec![3, 4, 3, 1, 2], input);
-
-    assert_eq!(1, simulate_fish(3, 0));
-    assert_eq!(1, simulate_fish(3, 1));
-    assert_eq!(1, simulate_fish(3, 2));
-    assert_eq!(1, simulate_fish(3, 3));
-    assert_eq!(2, simulate_fish(3, 4));
-    assert_eq!(2, simulate_fish(3, 5));
-    assert_eq!(2, simulate_fish(3, 6));
-    assert_eq!(2, simulate_fish(3, 7));
-    assert_eq!(2, simulate_fish(3, 8));
-    assert_eq!(2, simulate_fish(3, 9));
-    assert_eq!(2, simulate_fish(3, 10));
-    assert_eq!(3, simulate_fish(3, 11));
-    assert_eq!(3, simulate_fish(3, 12));
-    assert_eq!(4, simulate_fish(3, 13));
-    assert_eq!(4, simulate_fish(3, 14));
-    assert_eq!(4, simulate_fish(3, 15));
-    assert_eq!(4, simulate_fish(3, 16));
-    assert_eq!(4, simulate_fish(3, 17));
-    assert_eq!(5, simulate_fish(3, 18));
 
     assert_eq!(5, simulate_population(&input, 0));
     assert_eq!(5, simulate_population(&input, 1));
