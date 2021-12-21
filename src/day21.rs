35a7153f709ef9258b0680a2c9e5753d3af63150
Tim Peters <tim@catminion.net>
2021-12-21 17:45:29 +1030

    Refactor out duplicate structure

--- src/day21.rs
+++ src/day21.rs
@@ -16,8 +16,7 @@ pub fn run(input: &str, part: Part) -> String {
 
 #[derive(Debug)]
 struct Game {
-    pos: [usize; 2],
-    score: [usize; 2],
+    state: State,
     turn: usize,
     dice: Cycle<RangeInclusive<usize>>,
 }
@@ -32,10 +31,10 @@ impl Game {
     fn turn(&mut self) {
         let player = self.turn % 2;
         for _ in 0..3 {
-            self.pos[player] += self.dice.next().unwrap();
+            self.state.pos[player] += self.dice.next().unwrap();
         }
-        self.pos[player] = (self.pos[player] - 1) % 10 + 1;
-        self.score[player] += self.pos[player];
+        self.state.pos[player] = (self.state.pos[player] - 1) % 10 + 1;
+        self.state.score[player] += self.state.pos[player];
         self.turn += 1;
     }
 
@@ -50,18 +49,12 @@ impl Game {
     }
 
     fn winner(&self) -> Option<usize> {
-        if self.score[0] >= 1000 {
-            Some(0)
-        } else if self.score[1] >= 1000 {
-            Some(1)
-        } else {
-            None
-        }
+        self.state.score.iter().position(|&s| s >= 1000)
     }
 
     fn part1(&mut self) -> usize {
         self.play_until_end();
-        self.score[self.winner().unwrap() + 1 % 2] * self.rolls()
+        self.state.score[self.winner().unwrap() + 1 % 2] * self.rolls()
     }
 
     fn part2(&self) -> usize {
@@ -69,8 +62,8 @@ impl Game {
 
         let (p0_wins, p1_wins) = self.count_wins(
             State {
-                pos: self.pos,
-                score: self.score,
+                pos: self.state.pos,
+                score: self.state.score,
             },
             &mut seen,
         );
@@ -124,8 +117,10 @@ impl From<&str> for Game {
             .map(|line| line.chars().last().unwrap() as usize - 48)
             .collect::<Vec<_>>();
         Self {
-            pos: [start_pos[0], start_pos[1]],
-            score: [0, 0],
+            state: State {
+                pos: [start_pos[0], start_pos[1]],
+                score: [0, 0],
+            },
             turn: 0,
             dice: (1..=100).cycle(),
         }
@@ -139,31 +134,31 @@ Player 1 starting position: 4
 Player 2 starting position: 8
 ";
     let mut game = Game::from(test_input);
-    assert_eq!(4, game.pos[0]);
-    assert_eq!(0, game.score[0]);
-    assert_eq!(8, game.pos[1]);
-    assert_eq!(0, game.score[1]);
+    assert_eq!(4, game.state.pos[0]);
+    assert_eq!(0, game.state.score[0]);
+    assert_eq!(8, game.state.pos[1]);
+    assert_eq!(0, game.state.score[1]);
     assert_eq!(0, game.turn);
 
     game.turn();
-    assert_eq!(10, game.pos[0]);
-    assert_eq!(10, game.score[0]);
-    assert_eq!(8, game.pos[1]);
-    assert_eq!(0, game.score[1]);
+    assert_eq!(10, game.state.pos[0]);
+    assert_eq!(10, game.state.score[0]);
+    assert_eq!(8, game.state.pos[1]);
+    assert_eq!(0, game.state.score[1]);
     assert_eq!(1, game.turn);
 
     game.turn();
-    assert_eq!(10, game.pos[0]);
-    assert_eq!(10, game.score[0]);
-    assert_eq!(3, game.pos[1]);
-    assert_eq!(3, game.score[1]);
+    assert_eq!(10, game.state.pos[0]);
+    assert_eq!(10, game.state.score[0]);
+    assert_eq!(3, game.state.pos[1]);
+    assert_eq!(3, game.state.score[1]);
     assert_eq!(2, game.turn);
 
     game.play_until_end();
-    assert_eq!(10, game.pos[0]);
-    assert_eq!(1000, game.score[0]);
-    assert_eq!(3, game.pos[1]);
-    assert_eq!(745, game.score[1]);
+    assert_eq!(10, game.state.pos[0]);
+    assert_eq!(1000, game.state.score[0]);
+    assert_eq!(3, game.state.pos[1]);
+    assert_eq!(745, game.state.score[1]);
     assert_eq!(331, game.turn);
 
     let mut game = Game::from(test_input);
